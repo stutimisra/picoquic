4134bcc1fc13a09e570a60d4d676aff9e5395fd8
Nitin Gupta <nitingupta@cmu.edu>
2019-09-24 11:11:14 -0400

    Adding in the ICID packet processing code
    This is mostly a copy of the code from icid_monitor.cc minus the
    interaction with Xcache and queuing of tasks to the thread pool.

--- xcache/xcache_icid_handler.cc
+++ xcache/xcache_icid_handler.cc
@@ -1,7 +1,11 @@
 #include "xcache_icid_handler.h"
+#include "xcache_irq_table.h"
 
 #include <sys/types.h>      // bind
 #include <sys/socket.h>     // bind
+
+#include <iostream>
+#include <memory>
 
 XcacheICIDHandler::XcacheICIDHandler(XcacheQUICServer& server)
     : _server(server) {
@@ -13,22 +17,104 @@ XcacheICIDHandler::XcacheICIDHandler(XcacheQUICServer& server)
     }
 
     // Bind to ICID Handler Port
-    struct sockaddr_in addr;
-    addr.sin_family = AF_INET;
-    addr.sin_port = htons(ICID_HANDLER_PORT);
-    addr.sin_addr.s_addr = htons(INADDR_ANY);
-    if (bind(_sockfd, (struct sockaddr*)&addr, sizeof(addr))){
+    //struct sockaddr_in addr;
+    auto addr = std::make_unique<struct sockaddr_in>();
+    addr->sin_family = AF_INET;
+    addr->sin_port = htons(ICID_HANDLER_PORT);
+    addr->sin_addr.s_addr = htons(INADDR_ANY);
+    if (bind(sockfd, (struct sockaddr*)addr.get(), sizeof(sockaddr_in))) {
         throw "ERROR: binding to ICID Handler port";
     }
 
     _sockfd = sockfd;
 }
 
+XcacheICIDHandler::~XcacheICIDHandler() {
+    if (_sockfd != -1) {
+        close(_sockfd);
+    }
+}
+
 int XcacheICIDHandler::fd() {
     return _sockfd;
 }
 
-int XcacheICIDHandler::handleICIDRequest() {
-    // TODO: ICID packet parsing and action here
-    return 0;
+int XcacheICIDHandler::handleICIDRequest()
+{
+    char buffer[ICID_MAXBUF];
+    int ret;
+    bool fetch_needed = false;
+    auto irqtable = XcacheIRQTable::get_table();
+
+    std::cout << "ICIDHandler: waiting for incoming ICID packet" << std::endl;
+    ret = recvfrom(_sockfd, buffer, ICID_MAXBUF, 0, NULL, NULL);
+    if(ret <= 0) {
+        std::cout << "Error reading interest:"
+            << strerror(errno) << std::endl;
+        return -1;
+    }
+
+    // Read in the XIA Header here
+    // TODO: Add additional checks to ensure buffer contains XIA pkt
+    std::cout << "ICIDHandler: reading XIA header" << std::endl;
+    struct click_xia *xiah = (struct click_xia *)buffer;
+    Graph dst_dag;
+    dst_dag.from_wire_format(xiah->dnode, &xiah->node[0]);
+
+    Graph src_dag;
+    src_dag.from_wire_format(xiah->snode, &xiah->node[xiah->dnode]);
+
+    // Now find the ICID intent
+    // TODO: Handle exception thrown if intent is not an ICID
+    std::cout << "ICIDHandler: looking for ICID intent" << std::endl;
+    Node icid = dst_dag.intent_ICID();
+
+    // Convert into CID to check for
+    Node cid(XID_TYPE_CID, icid.id_string());
+    std::string cid_str = cid.to_string();
+
+    // Check if the CID is local. If yes, queue job to push it to caller
+    // TODO: Create new QUIC conenciton to src_dag and send chunk
+    /*
+    printf("ICIDMonitor: checking to see if CID is local\n");
+    if(ctrl->is_CID_local(cid_str) == true) {
+        // Schedule a job to have the chunk pushed to caller
+        printf("ICIDMonitor: queuing a push of found CID\n");
+        ICIDWorkRequestPtr work(
+                std::make_unique<ICIDPushRequest>(cid.to_string(),
+                    src_dag.dag_string()));
+        pool->queue_work(std::move(work));
+        return;
+    }
+    */
+    std::cout << "ICIDHandler: CID was not local" << std::endl;
+    // If not,
+    // We need to fetch only if the chunk is not already in IRQ table
+    if(irqtable->has_entry(cid_str) == false) {
+        std::cout << "ICIDHandler: CID not requested before" << std::endl;
+        fetch_needed = true;
+    }
+
+    // add CID and requestor address to irq_table
+    std::cout << "ICIDHandler: updating interest request table" << std::endl;
+    if(irqtable->add_fetch_request(cid_str, src_dag.dag_string()) == false) {
+        std::cout << "Error adding fetch request to table" << std::endl;
+        return -1;
+    }
+
+    // If we already sent an interest for this chunk, just wait for
+    // it to be satisfied.
+    // TODO: This prevents repeated requests from client. Is that OK?
+    if(!fetch_needed) {
+        std::cout << "ICIDHandler: skipping duplicate ICID" << std::endl;
+        return 0;
+    }
+
+    std::cout << "ICIDHandler: sending new ICID request" << std::endl;
+    // Send a new ICID request with our socket's address
+    sockaddr_x icid_addr;
+    dst_dag.fill_sockaddr(&icid_addr);
+    // TODO: This should have our AID DAG instead instead of recv_sock
+    //XinterestedInCID(recv_sock, &icid_addr);
+    std::cout << "ICIDHandler: done handling ICID request" << std::endl;
 }
--- xcache/xcache_icid_handler.h
+++ xcache/xcache_icid_handler.h
@@ -3,15 +3,17 @@
 
 #include "xcache_quic_server.h"
 
-#define ICID_HANDLER_PORT 7934
+#define ICID_HANDLER_PORT 7994
+#define ICID_MAXBUF 2048
 
 class XcacheICIDHandler {
 public:
     XcacheICIDHandler(XcacheQUICServer& server);
+    ~XcacheICIDHandler();
     int fd();
     int handleICIDRequest();
 private:
     XcacheQUICServer& _server;
-    int _sockfd;
+    int _sockfd = {-1};
 };
 #endif //_XCACHE_ICID_HANDLER_H
