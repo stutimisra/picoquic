2938fefbb4c0ac442787b2aa1d3ea08016ef9f50
Nitin Gupta <nitingupta@cmu.edu>
2019-10-03 18:19:23 -0400

    Whitespace cleanup. No code change.

--- picoquic/quicxiasock.hpp
+++ picoquic/quicxiasock.hpp
@@ -8,17 +8,17 @@
 #include <unordered_set>
 
 class QUICXIASocket {
-	public:
-		QUICXIASocket(const std::string& aid);
-		~QUICXIASocket();
-		GraphPtr serveCID(const std::string& cid);
-		bool unserveCID(const std::string& cid);
-		int fd();
+    public:
+        QUICXIASocket(const std::string& aid);
+        ~QUICXIASocket();
+        GraphPtr serveCID(const std::string& cid);
+        bool unserveCID(const std::string& cid);
+        int fd();
         int fillAddress(sockaddr_x& addr);
-	private:
-		std::string _aid;
-		std::unordered_set<std::string> cids_served;
-		int sockfd = -1;
-		GraphPtr our_addr;
+    private:
+        std::string _aid;
+        std::unordered_set<std::string> cids_served;
+        int sockfd = -1;
+        GraphPtr our_addr;
 };
 #endif //QUIC_XIA_SOCK_H
